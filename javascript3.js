1fef7ad38370894c3b82419d64127266a476f0dd
AntonLH <anton.larrahi@gmail.com>
2016-12-12 13:21:36 +0100

    commit

---
+++ javascript3.js
@@ -0,0 +1,7 @@
+ var hilabeteak = new Array("URTARRILA","OTSAILA","MARTXOA","APIRILA","MAIATZA","EKAINA","UZTAILA","ABUZTUA","IRAILA","URRIA","AZAROA","ABENDUA"); 
+ var array= new Array()
+ var cont=0;
+ while (cont<12){
+ 	alert(hilabeteak[cont]);
+ 	cont++;
+ }
