d617c6515624ddff2d051c92fc6d204985f37127
AntonLH <anton.larrahi@gmail.com>
2017-01-16 13:35:19 +0100

    commit

--- javascript10.js
+++ javascript10.js
@@ -1,8 +1,23 @@
 function palindromoa(testua) {
+testua=testua.toLowerCase();
 var array=testua.split("");
+var array2=testua.split("");
 alert(array.length);
-if (array===array.reverse()) {
-	alert("palindromoa da");
+array=array.reverse();
+var i=0;
+var kontadorea=0;
+while (i<array.length){
+if (array[i]===array2[i]) {
+	kontadorea++;
+}
+i++;
 }
 
-palindromoa("aabbaa");
+if (kontadorea===array.length) {
+	alert("palindromoa da");
+}
+else {
+	alert("ez da palindromoa");
+}
+}
+palindromoa("aUtoreerotua");
