ffa166bc95b4ca659e32e84eccde8de560a52a66
AntonLH <anton.larrahi@gmail.com>
2017-01-24 14:15:05 +0100

    commit

--- javascript12.js
+++ javascript12.js
@@ -24,8 +24,7 @@ erosketak[zenbakia-1][3]=totala;
 lerroa=zenbakia+"\t"+izena+"\t\t"+kopurua+"\t\t"+salneurria+"\t\t"+totala+"\n";
 
 if(zenbakia==1){
-idaztekoa="Zbkia\tProduktua\tKopurua\t\tSalneurria\tZenbatekoa\n";
-document.getElementById("tiketa").value=idaztekoa;
+document.getElementById("tiketa").value="Zbkia\tProduktua\tKopurua\t\tSalneurria\tZenbatekoa\n";
 }
 
 document.getElementById("tiketa").value=document.getElementById("tiketa").value+lerroa;
@@ -37,10 +36,28 @@ document.getElementById("zenbaki").value=zenbakia;
 document.getElementById("faktura").addEventListener("click", faktura);
 
 function faktura() {
+var guztizkoa=0;
+var BEZ=document.getElementById("BEZ").value;
+document.write("<h1>Erosketen Faktura</h1>");
 
-document.write("<h2>Erosketen faktura</h2>");
-document.write("<table><tr><th>Zbkia</th><th>Produktua</th><th>Salneurria</th><th>Kopurua</th><th>Zenbatekoa</th></tr>");
+document.write("<table border=\"1\"><tr><th>Zbkia</th><th>Produktua</th><th>Salneurria</th><th>Kopurua</th><th>Zenbatekoa</th></tr>");
+ for(var i=0;i<zenbakia-1;i++) {
+ document.write("<tr>");
+ 
+ document.write("<td>"+parseInt(i+1)+"</td>");
+ for(var j=0;j<4;j++) {
+  document.write("<td>");
+        document.write(erosketak[i][j]);
+        if(j==3) guztizkoa+=parseInt(erosketak[i][3]);
+        document.write("</td>");
+        }
+ document.write("</tr>"); 
+ } 
 document.write("</table>");
-document.write("<table><tr><th>BEZ=%"+BEZ+"</th><th>Guztizkoa</th></tr>");
+
+document.write("<table border=\"1\"><tr><th>Bez: %"+BEZ+"</th><th>Guztizkoa</th></tr>");
+guztizkoa+=guztizkoa*BEZ/100;
+document.write("<tr><th>"+(guztizkoa*BEZ/100).toFixed(2)+"</th><th>"+guztizkoa.toFixed(2)+"</th></tr>");
 document.write("</table>");
+ 
 }
